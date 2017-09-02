2846bd89497fdb3f0fb4ac5a226df6b60f8b3418
Chih Chiang Chen <john.c.chen@delta.com.tw>
2017-09-03 02:02:41 +0800

    debug mode

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def ebs():
     return render_template('ebs.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,threaded=True)
+    app.run(host='0.0.0.0',port=80,threaded=True,debug=True)
